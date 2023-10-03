7f2fd5f0d5ec10f577bb1cc02efb06dea3cf675b
JoshTapi <joshua.tapia@tecsup.edu.pe>
2023-10-03 18:24:59 -0500

    Initial Commit

---
+++ MisAutos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MisAutos
+//
+//  Created by Joshua Tapia on 3/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
